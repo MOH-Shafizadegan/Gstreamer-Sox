7656db140df3a320453a988a645f9766df31800f
MOH-Shafizadegan <mh.shafizadegan@gmail.com>
2023-07-08 16:58:00 +0330

    CMakeLists.txt file and build directory added.

---
+++ stream/build/CMakeFiles/main.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/main.dir/src/gst_video.c.o"
+  "CMakeFiles/main.dir/src/gst_video.c.o.d"
+  "bin/main"
+  "bin/main.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
