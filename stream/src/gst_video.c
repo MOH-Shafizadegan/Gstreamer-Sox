1c9fb64f0e7b127aabc3cdb8c0571104c22543b0
MOH-Shafizadegan <mh.shafizadegan@gmail.com>
2023-07-08 16:56:29 +0330

    The source file of the Gstreamer code added.

---
+++ stream/src/gst_video.c
@@ -0,0 +1,31 @@
+#include <gst/gst.h>
+
+int main(int argc, char *argv[]) {
+  GstElement *pipeline;
+  GstBus *bus;
+  GstMessage *msg;
+
+  /* Initialize GStreamer */
+  gst_init (&argc, &argv);
+
+  /* Build the pipeline */
+  pipeline = gst_parse_launch ("playbin uri=file:///home/mohsh/Desktop/test_video.mp4 video-sink=\"x264enc ! mpegtsmux ! hlssink location=./static/segment%05d.ts playlist-location=./static/playlist.m3u8\"", NULL);
+
+
+
+  /* Start playing */
+  gst_element_set_state (pipeline, GST_STATE_PLAYING);
+
+  /* Wait until error or EOS */
+  bus = gst_element_get_bus (pipeline);
+  msg = gst_bus_timed_pop_filtered (bus, GST_CLOCK_TIME_NONE, GST_MESSAGE_ERROR | GST_MESSAGE_EOS);
+
+  /* Free resources */
+  if (msg != NULL)
+    gst_message_unref (msg);
+  gst_object_unref (bus);
+  gst_element_set_state (pipeline, GST_STATE_NULL);
+  gst_object_unref (pipeline);
+  return 0;
+}
+
